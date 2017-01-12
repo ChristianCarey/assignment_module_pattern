0ae8c78f6c50d0759aa6affc0e33f231cae50e19
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-12 12:11:22 -0500

    Implement row creation

--- model.js
+++ model.js
@@ -3,12 +3,34 @@
 APP = APP || {};
 
 APP.Model = (function() {
-  
+
   var boardState;
 
   var init = function() {
-    boardState = new Array(2)
-  
+    _setupBoard(2, 4);
+
+  };
+
+  var _setupBoard = function(rowNums, colNums) {
+    var rows = _createRows(rowNums, colNums);
+    _populateRows(colNums, rows);
+    return rows;
+  };
+
+  var _createRows = function(rowNums, colNums) {
+    var rows = new Array(number);
+    rows.map(function() {
+      return new Array(colNums)
+    });
+    return rows;
+  };
+
+  var _populateRows(rows) {
+    rows.forEach(function(row) {
+      row.map(function()) {
+        new MoleHole;
+      };
+    });
   };
 
   var getBoardState = function() {
